f0f6573cec2937f47c4acef739f90f4fa67bd349
Irine Juliet <59999380+Irine-Juliet@users.noreply.github.com>
2024-02-27 08:15:23 -0500

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,10 +1,14 @@
 import sys
-import pickle
-from decimal import Decimal, InvalidOperation
+import logging
+import sqlalchemy
 from bank import Bank
-from account import Account, Checking, Savings
-from datetime import datetime, date
+from base import Base
+from datetime import datetime
+from sqlalchemy.orm import sessionmaker
+from decimal import Decimal, InvalidOperation
 from exceptions import OverdrawError, TransactionSequenceError, TransactionLimitError
+
+logging.basicConfig(filename='bank.log', level=logging.DEBUG, format='%(asctime)s|%(levelname)s|%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 class BankCLI:
     """
@@ -19,8 +23,16 @@ class BankCLI:
         """
         Initializes a new BankCLI instance.
         """
-        self.bank = Bank()
-        self.current_account = None
+        self._session = Session()  # Create a new session
+        self._bank = self._session.query(Bank).first()  # Query the database for the bank state
+        if self._bank:
+            logging.debug("Loaded from bank.db")
+        else:
+            # If there's no bank state in the database, create a new one
+            self._bank = Bank() 
+            self._session.add(self._bank)
+            self._session.commit()
+        self._current_account = None
         self._choices = {
             "1": self._open_account,
             "2": self._summary,
@@ -28,20 +40,17 @@ class BankCLI:
             "4": self._add_transaction,
             "5": self._list_transactions,
             "6": self._interest_and_fees,
-            "7": self._save,
-            "8": self._load,
-            "9": self._quit,
+            "7": self._quit,
         }
-
     def _display_menu(self):
         """
         Displays the main menu of the CLI.
         """
         current_account_display = "None"
-        if self.current_account:
-            account_type = type(self.current_account).__name__
-            account_number = str(self.current_account.account_number).zfill(9)
-            balance = self.current_account.balance.quantize(Decimal('0.01'))
+        if self._current_account:
+            account_type = type(self._current_account).__name__
+            account_number = str(self._current_account._account_number).zfill(9)
+            balance = self._current_account._balance.quantize(Decimal('0.01'))
             current_account_display = f"{account_type}#{account_number},\tbalance: ${balance:,.2f}"
 
         print(f"--------------------------------\n"
@@ -53,11 +62,9 @@ class BankCLI:
               f"4: add transaction\n"
               f"5: list transactions\n"
               f"6: interest and fees\n"
-              f"7: save\n"
-              f"8: load\n"
-              f"9: quit\n"
+              f"7: quit\n"
               f">", end="")
-
+        
     def run(self):
         """
         Runs the main loop of the CLI, processing user input.
@@ -73,21 +80,23 @@ class BankCLI:
         Opens a new bank account of the specified type (checking or savings).
         """
         account_type = input("Type of account? (checking/savings)\n>").lower()
-        account = self.bank.open_account(account_type)
-
+        account = self._bank.open_account(account_type, self._session)
+        self._session.commit()
+        logging.debug("Saved to bank.db")
+        
     def _summary(self):
         """
         Displays a summary of all accounts in the bank.
         """
-        self.bank.summary()
+        self._bank.summary(self._session)
 
     def _select_account(self):
         """
         Selects an account based on the user-input account number.
         """
         account_number = int(input("Enter account number\n>"))
-        account = self.bank.select_account(account_number)
-        self.current_account = account
+        account = self._bank.select_account(account_number, self._session)
+        self._current_account = account
 
     def _add_transaction(self):
         """
@@ -112,7 +121,9 @@ class BankCLI:
                     print("Please try again with a valid date in the format YYYY-MM-DD.")
 
             try:
-                self.current_account.add_transaction(amount, valid_date)
+                self._current_account.add_transaction(amount, self._session, valid_date)
+                self._session.commit()
+                logging.debug("Saved to bank.db")
             except OverdrawError as overdraw:
                 print(overdraw)
             except TransactionLimitError as limit_error:
@@ -129,11 +140,11 @@ class BankCLI:
         """
 
         try:
-            transactions = self.bank.list_transactions(self.current_account.account_number)
+            transactions = self._bank.list_transactions(self._current_account._account_number, self._session)
             for transaction in transactions:
-                print(f"{transaction.date}, ${transaction.amount:,.2f}")
+                print(f"{transaction._date}, ${transaction._amount:,.2f}")
         except AttributeError:
-             print("This command requires that you first select an account.")
+            print("This command requires that you first select an account.")
 
 
     def _interest_and_fees(self):
@@ -141,26 +152,13 @@ class BankCLI:
         Applies interest and fees to the currently selected account.
         """
         try:
-            self.current_account.apply_interest_and_fees()
+            self._current_account.apply_interest_and_fees(self._session)
+            self._session.commit()
+            logging.debug("Saved to bank.db")
         except AttributeError:
              print("This command requires that you first select an account.")
         except TransactionSequenceError as interest_error:
             print(interest_error)
-       
-    def _save(self):
-        """
-        Saves the current state of the bank (including all accounts and transactions) to a file.
-        """
-        with open('bank_data.pkl', 'wb') as f:
-            pickle.dump(self.bank, f)
-
-    def _load(self):
-        """
-        Loads the state of the bank from a file.
-        """
-        with open('bank_data.pkl', 'rb') as f:
-            self.bank = pickle.load(f)
-        self.current_account = None  
 
     def _quit(self):
         """
@@ -169,4 +167,23 @@ class BankCLI:
         sys.exit(0)
 
 if __name__ == "__main__":
-    BankCLI().run()
+
+    engine = sqlalchemy.create_engine("sqlite:///bank.db")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine) 
+
+    try:
+        BankCLI().run()
+    except Exception as e:
+        # Extract the exception type and message 
+        exception_type = e.__class__.__name__
+        exception_message = repr(str(e)).replace('\n', '\\n') 
+        logging.error(f"{exception_type}: {exception_message}")
+        # Inform the user
+        print("Sorry! Something unexpected happened. Check the logs or contact the developer for assistance.")
+        sys.exit(0)
+
+
+#INCORPORATE SQL ON GUI
+#CLEAN UP GUI
+# EDIT ESSAY
